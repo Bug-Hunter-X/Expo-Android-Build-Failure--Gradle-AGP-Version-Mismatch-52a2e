8851202db173b6b7e681ff77a948353b039be1dd
BugHunter-X <askzenai@gmail.com>
2025-01-02 05:22:06 +0100

    Add buildError.js file

---
+++ buildError.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo CLI to build an Android app and involves a mismatch between the Android Gradle Plugin (AGP) version and the Gradle version used in your project.  The Expo CLI might attempt to use an AGP version incompatible with the Gradle version, leading to build failures.
